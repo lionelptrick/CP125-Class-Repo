1e790c459f8e3100afcfe05e673fe2727e454568
lionelptrick <lionelpatrickpat@gmail.com>
2026-01-07 13:05:21 +0800

    Complete Lab 4: Lists 2

--- labs/lab04/exercise5/exercise5.py
+++ labs/lab04/exercise5/exercise5.py
@@ -1,11 +1,17 @@
 
-
 def find_momentum_days(prices):
+    if len(prices) < 2:
+        return []
+
+    changes = []
+    for i in range(1, len(changes)):
+        return True
     
     pass
 
 
 # Test
 prices = [100, 102, 105, 107, 106, 108, 112, 114]
+
 result = find_momentum_days(prices)
 print(f"Momentum days: {result}")  # Expected: [2, 5, 6]
